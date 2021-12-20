cf92b9d59a3ff7f8733c03c2444fb787b6de56b7
Tugbish <tugba.kursun@yahoo.com.tr>
2021-12-20 20:35:56 +1100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+
+const element = document.createElement('div');
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Tugba is the champion';
+document.body.append(newHeader);
+
+
+
+// element.textContent = "You've changed what's on the screen!";
+
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+//   li.textContent = 'Hi there!';
+//   console.log(li.textContent);
+// }
+// element.append(ul);
+
+
